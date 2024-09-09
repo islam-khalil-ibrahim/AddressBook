0b54b50e1fbde8d822087d0be11d5d074976553f
Microsoft <Microsoft@islam-deriyah>
2024-09-09 17:52:06 +0300

    edit

--- OneDrive/NetBeansProjects/chalproject1/src/main/java/com/mycompany/chalproject1/Chalproject1.java
+++ OneDrive/NetBeansProjects/chalproject1/src/main/java/com/mycompany/chalproject1/Chalproject1.java
@@ -7,30 +7,22 @@ package com.mycompany.chalproject1;
 import java.util.Scanner;
 
 
-/**
- *
- * @author Microsoft
- */
 public class Chalproject1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input: number of terms to generate
         System.out.print("Enter the number of terms in the Fibonacci sequence: ");
         int terms = scanner.nextInt();
 
-        // First two Fibonacci numbers
         int first = 0, second = 1;
 
         System.out.print("Fibonacci Sequence: " + first + ", " + second);
 
-        // Generate the Fibonacci sequence
         for (int i = 3; i <= terms; i++) {
             int next = first + second;
             System.out.print(", " + next);
 
-            // Move to the next two numbers in the sequence
             first = second;
             second = next;
         }
